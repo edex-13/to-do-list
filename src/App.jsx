89599579d029a00cc3daeb30c416b1d959d54204
ederson lopez <ederson12f@gmail.com>
2021-11-14 21:19:09 -0500

    TodoCounter component and styles

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { TodoCounter } from './components/TodoCounter';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <TodoCounter/>
+
+    </>
+  );
+}
+
+export default App;
