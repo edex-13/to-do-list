b5c5ffdfea0d1e2046c125738ed4f5f8586aad08
ederson lopez <ederson12f@gmail.com>
2021-11-14 21:33:24 -0500

    TodoList , TodoItem  component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import { TodoCounter } from './components/TodoCounter';
-import { TodoSearch } from './components/TodoSearch';
+import {TodoCounter} from './components/TodoCounter';
+import {TodoSearch} from './components/TodoSearch';
+import {TodoList} from './components/TodoList';
+import {TodoItem} from './components/TodoItem';
 import './App.css';
 
+const todos = [
+	{text: 'Cortar cebolla', completed: true},
+	{text: 'Tomar el cursso de intro a React', completed: false},
+	{text: 'Llorar con la llorona', completed: false},
+	{text: 'LALALALAA', completed: false},
+];
+
 function App() {
-  return (
-    <>
-      <TodoCounter/>
-      <TodoSearch/>
-      {/* <TodoList>
-        <TodoItem/>
-        <TodoItem/>
-       </TodoList>
-       <CreateTodoButton/> */}
-    </>
-  );
+	return (
+		<>
+			<TodoCounter />
+			<TodoSearch />
+			<TodoList>
+				{todos.map((todo) => (
+					<TodoItem key={todo.text} text={todo.text} completed={todo.completed} />
+				))}
+			</TodoList>
+			{/* <CreateTodoButton/> */}
+		</>
+	);
 }
 
 export default App;
