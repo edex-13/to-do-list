ddcd5c469639c4f8365d2f1ccf8bf9f52e9f5f35
ederson lopez <ederson12f@gmail.com>
2021-11-14 21:43:23 -0500

    CreateTodoButton component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {TodoCounter} from './components/TodoCounter';
 import {TodoSearch} from './components/TodoSearch';
 import {TodoList} from './components/TodoList';
 import {TodoItem} from './components/TodoItem';
+import {CreateTodoButton} from './components/CreateTodoButton';
 import './App.css';
 
 const todos = [
@@ -21,7 +22,7 @@ function App() {
 					<TodoItem key={todo.text} text={todo.text} completed={todo.completed} />
 				))}
 			</TodoList>
-			{/* <CreateTodoButton/> */}
+			<CreateTodoButton/>
 		</>
 	);
 }
---
+++ src/components/CreateTodoButton.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../styles/CreateTodoButton.css';
+
+const CreateTodoButton = () => {
+	return <button className="CreateTodoButton">+</button>;
+};
+export { CreateTodoButton };
