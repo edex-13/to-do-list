4db03e7dcaae100dad6b743284141acb214a6f73
ederson lopez <ederson12f@gmail.com>
2021-11-14 21:26:58 -0500

    TodoSearch component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { TodoCounter } from './components/TodoCounter';
+import { TodoSearch } from './components/TodoSearch';
 import './App.css';
 
 function App() {
   return (
     <>
       <TodoCounter/>
-
+      <TodoSearch/>
+      {/* <TodoList>
+        <TodoItem/>
+        <TodoItem/>
+       </TodoList>
+       <CreateTodoButton/> */}
     </>
   );
 }
